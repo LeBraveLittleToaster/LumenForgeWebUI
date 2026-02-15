de293189e28fab75224a7c0931a53b48e749c13a
Pascal Schiessle <pascal.schiessle@web.de>
2026-02-15 20:41:59 +0100

    [feat] Toolbar router

--- lumenforgewebui/src/app/app.ts
+++ lumenforgewebui/src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
+import { AuthService } from './core/auth/auth-service';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,8 @@ import { MatToolbar } from '@angular/material/toolbar';
   styleUrl: './app.css'
 })
 export class App {
+  private readonly auth = inject(AuthService);
+  readonly isAuthenticated = this.auth.isAuthenticated();
   protected readonly title = signal('lumenforgewebui');
+  
 }
--- lumenforgewebui/src/app/features/home/home.ts
+++ lumenforgewebui/src/app/features/home/home.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/auth/auth-service';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.scss',
 })
 export class Home {
-
+  readonly firstName = inject(AuthService).user()?.firstName ?? 'Guest';
+  readonly lastName = inject(AuthService).user()?.lastName ?? '';
 }
